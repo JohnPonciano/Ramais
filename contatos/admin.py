6b729e8b342946eebe9d5cd0b0c0e212b3a6c9d5
johnponciano <jhow.ponciano@hotmail.com>
2021-11-23 16:55:39 -0300

    Add Contacts in Ramal lists

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Contato
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ("nome","sobrenome","telefone","categoria")
+    list_display_links= ("nome","sobrenome","telefone")
+    list_filter = ["categoria"]
+    search_fields= ("nome","sobrenome")
+    list_per_page= 10
+    
+
+admin.site.register(Categoria)
+admin.site.register(Contato,ContatoAdmin)
+
--- contatos/views.py
+++ contatos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import Contato
 # Create your views here.
 
 def index(request):
-    return render(request,'contatos/index.html')
+    contatos = Contato.objects.all()
+    return render(request,'contatos/index.html',{
+        'contatos': contatos
+    })
